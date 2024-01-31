0a85bc30309aea3210479448b4e9d9123f5421b8
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-01-31 21:07:52 +0100

    commit

--- assets/nav-controller.js
+++ assets/nav-controller.js
@@ -7,10 +7,12 @@ window.addEventListener("scroll", function () {
     if (window.scrollY >= 100) {
         navColorControl.classList.add("color-background-1");
         navColorControl.classList.remove("color-background-2");
+        navColorControl.style.backgroundColor = "white";
         logoColor.style.filter = "brightness(0)";
     } else {
         logoColor.style.filter = "brightness(1)";
         navColorControl.classList.remove("color-background-1");
         navColorControl.classList.add("color-background-2");
+        navColorControl.style.backgroundColor = "none";
     }
 });
