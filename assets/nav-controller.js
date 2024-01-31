b43a777a938892030787732d4b69eb6e64db7459
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-01-31 16:12:23 +0100

    commit

--- assets/nav-controller.js
+++ assets/nav-controller.js
@@ -1,6 +1,6 @@
 const navColorControl = document.querySelector(".header--middle-left");
 
 if (screen.height > 0) {
-    navColorControl.style.backgroundColor = "red";
+    navColorControl.style.backgroundColor = "";
 } else {
 }
