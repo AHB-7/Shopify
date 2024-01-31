5597bea99ce4c7e9cb06242bbef18d6befba7c83
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-01-31 22:59:51 +0100

    commit

--- assets/nav-controller.js
+++ assets/nav-controller.js
@@ -1,19 +1,20 @@
 const navColorControl = document.querySelector("sticky-header");
 const logoColor = document.querySelector(".header__heading-logo-wrapper");
+const backgroundColorControl = document.querySelector(".header - wrapper");
 
 logoColor.style.filter = "brightness(1)";
-navColorControl.style.backgroundColor = "none";
+backgroundColorControl.style.backgroundColor = "none";
 
 window.addEventListener("scroll", function () {
     if (window.scrollY >= 100) {
         navColorControl.classList.add("color-background-1");
         navColorControl.classList.remove("color-background-2");
-        navColorControl.style.backgroundColor = "white";
+        backgroundColorControl.style.backgroundColor = "white";
         logoColor.style.filter = "brightness(0)";
     } else {
         logoColor.style.filter = "brightness(1)";
         navColorControl.classList.remove("color-background-1");
         navColorControl.classList.add("color-background-2");
-        navColorControl.style.backgroundColor = "none";
+        backgroundColorControl.style.backgroundColor = "none";
     }
 });
