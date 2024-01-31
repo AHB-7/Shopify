cf5815ab89ecf3ba0b59d600cc3f3dc5c5e30758
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-01-31 17:15:11 +0100

    commit

--- assets/nav-controller.js
+++ assets/nav-controller.js
@@ -1,7 +1,7 @@
 const navColorControl = document.querySelector(".header--middle-left");
 const logoColor = document.querySelector(".header__heading-logo-wrapper");
 if (screen.height > 0) {
-    navColorControl.style.backgroundColor = "";
+    navColorControl.style.filter = "brightness(1)";
     logoColor.style.filter = "brightness(1)";
 } else {
 }
