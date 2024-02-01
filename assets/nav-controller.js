dd126af4505193f1c04ee755ec5bed24b459412b
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-02-01 18:48:29 +0100

    commit

--- assets/nav-controller.js
+++ assets/nav-controller.js
@@ -1,4 +1,4 @@
-const navColorControl = document.querySelector("sticky-header");
+const navColorControl = document.querySelector(".sticky-header");
 const logoColor = document.querySelector(".header__heading-logo-wrapper");
 const backgroundColorControl = document.querySelector(".header-wrapper");
 
@@ -6,11 +6,21 @@ logoColor.style.filter = "brightness(1)";
 backgroundColorControl.style.backgroundColor = "none";
 
 window.addEventListener("scroll", function () {
+    const currentUrl = window.location.href;
+
     if (window.scrollY >= 100) {
-        navColorControl.classList.add("color-background-1");
-        navColorControl.classList.remove("color-background-2");
-        backgroundColorControl.style.backgroundColor = "white";
-        logoColor.style.filter = "brightness(0)";
+        if (currentUrl.includes("velissi.com")) {
+            navColorControl.classList.add("color-background-1");
+            navColorControl.classList.remove("color-background-2");
+            backgroundColorControl.style.backgroundColor = "white";
+            logoColor.style.filter = "brightness(0)";
+        } else {
+            // Handle other URLs here
+            // For now, using the same behavior as when scrolling up on velissi.com
+            navColorControl.classList.add("color-background-1");
+            navColorControl.classList.remove("color-background-2");
+            logoColor.style.filter = "brightness(0)";
+        }
     } else {
         logoColor.style.filter = "brightness(1)";
         navColorControl.classList.remove("color-background-1");
@@ -18,9 +28,5 @@ window.addEventListener("scroll", function () {
         backgroundColorControl.style.backgroundColor = "transparent";
     }
 });
-const currentUrl = window.location.href;
-if (currentUrl != velissi.com) {
-    navColorControl.classList.add("color-background-1");
-    navColorControl.classList.remove("color-background-2");
-}
+
 console.log(currentUrl);
