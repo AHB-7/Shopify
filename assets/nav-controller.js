0ec8b32aa8cd2b3251f80e9c2e090a9324b0fac5
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-02-01 19:03:06 +0100

    commit

--- assets/nav-controller.js
+++ assets/nav-controller.js
@@ -1,6 +1,7 @@
 const navColorControl = document.querySelector("sticky-header");
 const logoColor = document.querySelector(".header__heading-logo-wrapper");
 const backgroundColorControl = document.querySelector(".header-wrapper");
+const currentUrl = window.location.pathname;
 
 logoColor.style.filter = "brightness(1)";
 backgroundColorControl.style.backgroundColor = "none";
@@ -12,15 +13,14 @@ window.addEventListener("scroll", function () {
         backgroundColorControl.style.backgroundColor = "white";
         logoColor.style.filter = "brightness(0)";
     } else {
+        if (currentUrl.length >= 1) {
+            logoColor.style.filter = "brightness(0)";
+            navColorControl.classList.add("color-background-1");
+            navColorControl.classList.remove("color-background-2");
+        }
         logoColor.style.filter = "brightness(1)";
         navColorControl.classList.remove("color-background-1");
         navColorControl.classList.add("color-background-2");
         backgroundColorControl.style.backgroundColor = "transparent";
     }
 });
-const currentUrl = window.location.pathname;
-// if (currentUrl === velissi.com) {
-//     navColorControl.classList.add("color-background-1");
-//     navColorControl.classList.remove("color-background-2");
-// }
-console.log(currentUrl);
