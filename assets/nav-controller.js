7184eb819904e94c8fc670a9435b305ad64f7681
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-02-04 16:52:04 +0100

    commit

--- assets/nav-controller.js
+++ assets/nav-controller.js
@@ -13,7 +13,7 @@ backgroundColorControl.style.backgroundColor = "none";
 if (currentUrl !== "/") {
     logoColor.style.filter = "brightness(0)";
     navColorControl.classList.add("color-background-1");
-    navColorControl.classList.remove("color-background-4");
+    navColorControl.classList.remove("color-background-2");
 } else {
     logoColor.style.filter = "brightness(1)";
 }
@@ -21,18 +21,18 @@ if (currentUrl !== "/") {
 window.addEventListener("scroll", function () {
     if (window.scrollY >= 100) {
         navColorControl.classList.add("color-background-1");
-        navColorControl.classList.remove("color-background-4");
+        navColorControl.classList.remove("color-background-2");
         backgroundColorControl.style.backgroundColor = "white";
         logoColor.style.filter = "brightness(0)";
     } else {
         if (currentUrl !== "/") {
             logoColor.style.filter = "brightness(0)";
             navColorControl.classList.add("color-background-1");
-            navColorControl.classList.remove("color-background-4");
+            navColorControl.classList.remove("color-background-2");
         } else {
             logoColor.style.filter = "brightness(1)";
             navColorControl.classList.remove("color-background-1");
-            navColorControl.classList.add("color-background-4");
+            navColorControl.classList.add("color-background-2");
             backgroundColorControl.style.backgroundColor = "transparent";
         }
     }
